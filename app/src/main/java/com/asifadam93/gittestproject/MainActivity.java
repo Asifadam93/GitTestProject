e8bf0b5e2ca8bad463b32f34ef4f035e41642684
fisamada39 <asifadam93@hotmail.fr>
2016-10-07 22:59:52 +0200

    Hello world log added to mainActivity

--- app/src/main/java/com/asifadam93/gittestproject/MainActivity.java
+++ app/src/main/java/com/asifadam93/gittestproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.asifadam93.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void testFunction(){
+
+        Log.i("MainActivity","Hello world !");
+
     }
 }
