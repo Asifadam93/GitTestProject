4925d4cf361dd1d718145316eaa0a0ecb2d30d8b
fisamada39 <asifadam93@hotmail.fr>
2016-10-08 14:07:05 +0200

    Fabric : App traker installed !

--- app/src/main/java/com/asifadam93/gittestproject/MainActivity.java
+++ app/src/main/java/com/asifadam93/gittestproject/MainActivity.java
@@ -3,12 +3,15 @@ package com.asifadam93.gittestproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 
